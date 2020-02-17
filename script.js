cdb0daf19ea608cc156b1a0ad8a15331ec6fadfa
mathias <math06j6@stud.kea.dk>
2020-02-17 12:23:14 +0100

    v2

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 "use strict";
-window.addEventListener("DOMContentLoaded", start);
-const elementInput = document.querySelector("#input");
-const dropDown = document.querySelector("#list");
-const elementOutput = document.querySelector("#output");
-const btnActivate = document.querySelector("button");
-let valueSelected;
-let inputValue;
-let newValue;
+
+document.addEventListener("DOMContentLoaded", start);
+
+const myArray =[];
 
 function start() {
-  console.log("start");
 
-  btnActivate.addEventListener("click", userOptions);
+  let random = (Math.floor(Math.random() * 32));
+  if (myArray.length < 40)  {
+    myArray.push(random);
+    console.log("push");
+    //move along - nothing to see here
+    setHeight(myArray.length-1);
+  } else {
+    //Remove one entriy from end of array.
+    myArray.shift();
+    console.log("shift");
+    start();
+
+  }
+console.log(myArray.length-1)
+  
 }
 
-function userOptions() {}
+function setHeight(number) {
+  
+ // for (let number = 0; number < myArray.length; number++) {
+
+    const partOfBar = document.createElement("div");
+    partOfBar.classList.add("bar");
+    document.querySelector("#container").appendChild(partOfBar);
+  console.log(myArray[number]);
+    let bar;
+    bar = document.querySelector(`#container > div:nth-child(${number + 1})`);
+    bar.style.setProperty("--height", myArray[number]);
+ // }
+
+
+  // goes to delay function
+  setTimeout(start, 100);
+}
+
